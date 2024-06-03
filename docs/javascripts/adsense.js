50d033af8eaef3eb228c5a5615c57e30e5d4072c
Braundo <aaronbraundmeier@gmail.com>
2024-06-03 16:50:26 -0500

    some google adsense stuff

---
+++ docs/javascripts/adsense.js
@@ -0,0 +1,14 @@
+// adsense.js
+(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4744916432619667",
+    enable_page_level_ads: true
+  });
+  
+  (function() {
+    var adsenseScript = document.createElement('script');
+    adsenseScript.async = true;
+    adsenseScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    adsenseScript.crossOrigin = "anonymous";
+    document.head.appendChild(adsenseScript);
+  })();
+  
